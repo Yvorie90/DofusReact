e839da8cf553b2d749792a3277110e0e0207927c
Jolan <jolan.renoux@etu-nantes.fr>
2022-11-30 08:56:29 +0100

    ✨ about

---
+++ src/components/AboutContent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AboutContent = () => {
+    return (
+        <p className="about-content">
+            Ce site est un projet JS React <br />
+            développé par Yvorie <br />
+            hébergé sur Github et Netlify
+        </p>
+    );
+};
+
+export default AboutContent;
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import AboutContent from '../components/AboutContent';
 import Navbar from "../components/Navbar"
 
 const About = () => {
@@ -7,9 +8,7 @@ const About = () => {
             <div className='navbar-container'>
                 <Navbar/>
                 <div className='content-page'>
-                    <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam suscipit officia, asperiores quod sapiente, sint perferendis nisi dolor necessitatibus non perspiciatis unde id, ad saepe quis! Facilis, aut dolor sit cupiditate quisquam provident, earum, soluta officiis pariatur impedit nisi! Veritatis eligendi dolorum perferendis. Modi accusantium minima voluptatibus laboriosam tenetur odio! Fugit excepturi illo temporibus in accusantium tempora, dicta similique incidunt ut sint quo ab cum harum earum. Assumenda aperiam, pariatur reiciendis sed cum ipsam, consequatur nemo qui voluptatibus quia excepturi porro ullam deleniti laudantium, obcaecati architecto placeat magnam. Veniam placeat doloribus amet fugit itaque ipsa temporibus aliquid corrupti, maxime nihil.
-                    </p>
+                    <AboutContent/>
                 </div>
             </div>
         </div>
