e6f45f1fc5ea46c0f3746a26a1fc2ea9cb3895cd
Jolan <jolan.renoux@etu-nantes.fr>
2022-12-01 00:17:38 +0100

    ♻️ refacto import

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,4 @@
-import React, { useEffect } from 'react';
-import {useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import infoH from '../img/info-hover.png'
 
 const Card = ({item}) => {
