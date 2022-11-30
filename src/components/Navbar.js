5911b09abda872730e1377669d5c9cc8944a97ab
Yvorie <E208633A@univ-nantes.fr>
2022-11-30 17:09:04 +0100

    ✨ order navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,11 +9,11 @@ const Navbar = () => {
                 <NavLink to='/' className={(nav) => (nav.isActive ? "nav-active" : "")}>
                     <li> <img src="https://img.icons8.com/ios-filled/50/FFFFFF/online-store.png" alt='home'/> </li>
                 </NavLink>
-                <NavLink to='/about' className={(nav) => (nav.isActive ? "nav-active" : "")}>
-                    <li> <img className='img-navbar' src="https://img.icons8.com/pastel-glyph/64/FFFFFF/information--v1.png" alt='about'/> </li>
-                </NavLink>
                 <NavLink to='/inventory' className={(nav) => (nav.isActive ? "nav-active" : "")}>
                     <li> <img className='img-navbar' src="https://img.icons8.com/ios-glyphs/90/FFFFFF/user--v1.png" alt='inventory'/> </li>
+                </NavLink>
+                <NavLink to='/about' className={(nav) => (nav.isActive ? "nav-active" : "")}>
+                    <li> <img className='img-navbar' src="https://img.icons8.com/pastel-glyph/64/FFFFFF/information--v1.png" alt='about'/> </li>
                 </NavLink>
             </ul>
         </div>
